4d9a2ee9dec2e0cd9b9094b4ef12e4cc4d03bafb
Dominic Torruellas <dom@domtorr.com>
2020-02-17 22:37:21 -0700

    Chapter 05 done

---
+++ 05-Using-Structs/01-Defining-and-Instantiating-Structs/defining_structs/src/main.rs
@@ -0,0 +1,41 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("user1 email: {}", user1.email);
+
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user2.email = String::from("anotheremail@example.com");
+    println!("user2 email: {}", user2.email);
+
+    let user3 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user2
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
