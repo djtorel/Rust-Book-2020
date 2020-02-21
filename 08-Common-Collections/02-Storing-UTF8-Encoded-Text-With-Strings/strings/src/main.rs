f3cf1840db2b2b9b504e77ebe4dc131925c21f60
Dominic Torruellas <dom@domtorr.com>
2020-02-20 22:55:48 -0700

    Chapter 08 done

---
+++ 08-Common-Collections/02-Storing-UTF8-Encoded-Text-With-Strings/strings/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    let s = "initial contents".to_string();
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    // let s = s1 + "-" + &s2 + "-" + &s3;
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("Hello, world!");
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+}
---
+++ 08-Common-Collections/03-Storing-Keys-and-Values-In-Hashmaps/hashmaps/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{:#?}", scores);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:#?}", scores);
+
+    let text = "Hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:#?}", map);
+}
